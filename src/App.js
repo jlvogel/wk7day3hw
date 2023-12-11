3f4893ba34f8b783eecb0b1d6bdda69365f0174f
Joshua Vogel <40187940+jlvogel@users.noreply.github.com>
2023-12-11 02:20:38 -0500

    Basic component tree added to App.js  Next step is to create individual component files inside src/components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      {/* <HomePage>
+        <Header></Header>
+        <SearchBar></SearchBar>
+        <EmployeeList>
+          <EmployeeListItem></EmployeeListItem>
+          <EmployeeListItem></EmployeeListItem>
+          <EmployeeListItem></EmployeeListItem>
+          <EmployeeListItem></EmployeeListItem>
+          <EmployeeListItem></EmployeeListItem>
+          <EmployeeListItem></EmployeeListItem>
+        </EmployeeList>
+      </HomePage>
+      <EmployeePage>
+        <Header></Header>
+        <Employee>
+          <EmployeeListItem></EmployeeListItem>
+          <EmployeeItem></EmployeeItem>
+          <EmployeeItem></EmployeeItem>
+          <EmployeeItem></EmployeeItem>
+          <EmployeeItem></EmployeeItem>
+        </Employee>
+      </EmployeePage> */}
     </div>
   );
 }
